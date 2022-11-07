a8fa301aee797a11817270b4f20958a9564eded8
Jean Porto <portojean@gmail.com>
2022-11-07 10:00:58 -0300

    exercicios sobre condicionais

---
+++ DIO - Aprendendo o que eh Ruby/condicionais3.rb
@@ -0,0 +1,9 @@
+#https://web.dio.me/course/aprendendo-o-que-e-ruby/learning/ead6ed3a-87ac-4f62-9ca5-20b27d54b4ac
+#DIO - Aprendendo o que Ruby - Condicionais
+
+puts "Digitar um valor:"
+valor = gets.chomp.to_i
+
+unless valor == 100
+    puts "o valor não é 100"
+end
---
+++ DIO - Aprendendo o que eh Ruby/condicionais4.rb
@@ -0,0 +1,7 @@
+#https://web.dio.me/course/aprendendo-o-que-e-ruby/learning/ead6ed3a-87ac-4f62-9ca5-20b27d54b4ac
+#DIO - Aprendendo o que Ruby - Condicionais
+
+puts "Digitar um valor:"
+valor = gets.chomp.to_i
+
+puts "Eu sou maior que 50" if valor > 50
