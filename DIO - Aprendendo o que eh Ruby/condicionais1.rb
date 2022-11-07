4d6b3660093b96e390f544b4fd48cfc8667bfe44
Jean Porto <portojean@gmail.com>
2022-11-07 09:42:27 -0300

    mais uma exemplo sobre condicionais

---
+++ DIO - Aprendendo o que eh Ruby/condicionais1.rb
@@ -0,0 +1,6 @@
+#https://web.dio.me/course/aprendendo-o-que-e-ruby/learning/ead6ed3a-87ac-4f62-9ca5-20b27d54b4ac
+#DIO - Aprendendo o que Ruby - Condicionais
+
+valor = gets.chomp.to_i
+
+p valor.class 
