e322e3a7356aa4baf4fdaa8473171aa85113b693
Jean Porto <portojean@gmail.com>
2022-11-08 08:30:46 -0300

    refactor: metodo com parametro default

--- DIO - Aprendendo o que eh Ruby/metodo.rb
+++ DIO - Aprendendo o que eh Ruby/metodo.rb
@@ -7,5 +7,14 @@ def soma(valor1, valor2)
     valor1 + valor2
 end
 
-
 puts soma(10, 5)
+
+
+#colocando valor no segundo parametro - ficar como opcional
+def soma_opcional(valor1, valor2=0)
+    puts "Soma com 2 parametro opcional #{valor1} e #{valor2}"
+
+    valor1 + valor2
+end
+
+puts soma_opcional(10)
