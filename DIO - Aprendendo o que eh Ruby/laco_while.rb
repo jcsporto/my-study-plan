4f39182b76615cb7a0c9ba8bf65a34bad30e3363
Jean Porto <portojean@gmail.com>
2022-11-08 07:15:41 -0300

    add o break

--- DIO - Aprendendo o que eh Ruby/laco_while.rb
+++ DIO - Aprendendo o que eh Ruby/laco_while.rb
@@ -2,8 +2,11 @@
 #https://web.dio.me/course/aprendendo-o-que-e-ruby/learning/d8c0b891-4bfe-4c7a-8fd8-5394a3137682
 #DIO - Aprendendo o que é Ruby - Laços
 
-valor = 5
-while valor > 0
-    puts valor
-    valor -= 1
+valor = 0
+while valor < 10
+    puts "O valor é: #{valor}"
+
+    break if valor == 5
+
+    valor += 1
 end
