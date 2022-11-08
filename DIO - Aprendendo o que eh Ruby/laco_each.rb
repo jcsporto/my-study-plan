deff0e8f7edb78a613975c4d0c963eea1cd010a1
Jean Porto <portojean@gmail.com>
2022-11-08 07:31:48 -0300

    feat: usando each

---
+++ DIO - Aprendendo o que eh Ruby/laco_each.rb
@@ -0,0 +1,18 @@
+#https://web.dio.me/course/aprendendo-o-que-e-ruby/learning/d8c0b891-4bfe-4c7a-8fd8-5394a3137682
+#DIO - Aprendendo o que é Ruby - Laços
+
+#exemplo do for
+lista = [0, 1, 2, 3, 4, 5]
+
+for i in lista
+    puts "usando for: #{i}"
+end
+
+
+#aplicando o each
+lista.each do |i|
+    puts "usando each: #{i}"
+end
+
+
+
