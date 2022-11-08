aec06f844c7410eb40678af832dc34f53acf33d1
Jean Porto <portojean@gmail.com>
2022-11-08 07:03:24 -0300

    refactor While

---
+++ laco_while.rb
@@ -0,0 +1,9 @@
+
+#https://web.dio.me/course/aprendendo-o-que-e-ruby/learning/d8c0b891-4bfe-4c7a-8fd8-5394a3137682
+#DIO - Aprendendo o que é Ruby - Laços
+
+valor = 5
+while valor > 0
+    puts valor
+    valor -= 1
+end
