54e4ae1d8cafd83ce4179966b5f7fbe9e2917e9f
Jean Porto <portojean@gmail.com>
2022-11-08 08:27:50 -0300

    feat: metodo soma

---
+++ DIO - Aprendendo o que eh Ruby/metodo.rb
@@ -0,0 +1,11 @@
+#https://web.dio.me/course/aprendendo-o-que-e-ruby/learning/5b7cbe04-57a1-46c2-8bfe-91454801caa2
+##DIO - Aprendendo o que Ruby - Metodos
+
+def soma(valor1, valor2)
+    puts "Soma de #{valor1} e #{valor2}"
+
+    return valor1 + valor2
+end
+
+
+puts soma(10, 5)
