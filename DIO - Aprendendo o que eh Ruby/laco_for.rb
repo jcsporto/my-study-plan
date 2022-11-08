059e7f18b34295c3dc591222b607e5070c6d8095
Jean Porto <portojean@gmail.com>
2022-11-08 07:08:16 -0300

    for

---
+++ DIO - Aprendendo o que eh Ruby/laco_for.rb
@@ -0,0 +1,7 @@
+#https://web.dio.me/course/aprendendo-o-que-e-ruby/learning/d8c0b891-4bfe-4c7a-8fd8-5394a3137682
+#DIO - Aprendendo o que é Ruby - Laços
+
+
+for i in [1, 2, 3, 4, 5]
+    puts "O número é #{i}"
+end
