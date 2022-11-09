0e450b53595e2dcc9cda32542d86cdb495ce9c61
Jean Porto <portojean@gmail.com>
2022-11-09 08:21:11 -0300

    fix: consertando a classe pessoa

---
+++ DIO -  Ruby - Introdução a orientação a objetos/classes/pessoa.rb
@@ -0,0 +1,7 @@
+#https://web.dio.me/course/ruby-introducao-a-orientacao-a-objetos/learning/19cc056e-c8cf-4cf5-b30c-703f8e4305d9
+#DIO - Ruby: Introdução a orientação a objeto
+class Pessoa
+    def initialize(nome)
+        @nome = nome
+    end
+end
