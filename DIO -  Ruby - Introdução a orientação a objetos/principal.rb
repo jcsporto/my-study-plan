bc457a3c97f2bb50ecbd3ca6bba82fa355e61ced
Jean Porto <portojean@gmail.com>
2022-11-09 08:26:24 -0300

    feat: principal

--- DIO -  Ruby - Introdução a orientação a objetos/principal.rb
+++ DIO -  Ruby - Introdução a orientação a objetos/principal.rb
@@ -1,2 +1,10 @@
 #https://web.dio.me/course/ruby-introducao-a-orientacao-a-objetos/learning/19cc056e-c8cf-4cf5-b30c-703f8e4305d9
 #DIO - Ruby: Introdução a orientação a objeto
+
+require "./classes/pessoa"
+require "./classes/carro"
+
+puts "Executando Principal"
+
+foo = Pessoa.new("Foo")
+carro = Carro.new("modelo", foo)
