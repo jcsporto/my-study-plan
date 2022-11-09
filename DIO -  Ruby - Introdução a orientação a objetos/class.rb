e8ef59f94dd1406e58225ef36251ff0b0fc838e4
Jean Porto <portojean@gmail.com>
2022-11-09 07:59:24 -0300

    add attr_accessor

--- DIO -  Ruby - Introdução a orientação a objetos/class.rb
+++ DIO -  Ruby - Introdução a orientação a objetos/class.rb
@@ -1,7 +1,29 @@
 class MinhaClasse
+    attr_accessor :nome #getter e setter
+    #attr_reader :nome # apenas getter
+    #attr_writter :nome # apenas setter
 
+    def initialize(nome)
+        @nome = nome
+    end
+
+    def imprimir_ola(nome)
+        @nome = nome 
+        puts "Ola #{@nome}"
+    end
+
+    # #getrs
+    # def nome
+    #     @nome
+    # end
+
+    # #setrs
+    # def nome=(novo_nome)
+    #     @nome = novo_nome
+    # end
 end
 
-objeto = MinhaClasse.new
 
-p objeto.object_id
+pessoa = MinhaClasse.new("Jean")
+pessoa.nome = "Porto"
+pessoa.imprimir_ola
