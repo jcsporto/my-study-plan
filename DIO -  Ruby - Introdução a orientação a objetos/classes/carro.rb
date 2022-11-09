05f3dd2ba5babe5fcf293db242f29043840bb344
Jean Porto <portojean@gmail.com>
2022-11-09 08:20:28 -0300

    fix: consertando a classe carro

--- DIO -  Ruby - Introdução a orientação a objetos/classes/carro.rb
+++ DIO -  Ruby - Introdução a orientação a objetos/classes/carro.rb
@@ -1,8 +1,9 @@
 #https://web.dio.me/course/ruby-introducao-a-orientacao-a-objetos/learning/19cc056e-c8cf-4cf5-b30c-703f8e4305d9
 #DIO - Ruby: Introdução a orientação a objeto
 
-class Pessoa
-    def initialize(nome)
-        @nome = nome
+class Carro
+    def initialize(modelo, dono)
+        @modelo = modelo
+        @dono = dono
     end
 end
