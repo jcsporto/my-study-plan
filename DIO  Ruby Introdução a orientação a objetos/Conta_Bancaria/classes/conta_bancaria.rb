4a1d3f1ff8a6d85b49c3104f0141cc05a0826601
Jean Porto <portojean@gmail.com>
2022-11-10 08:54:55 -0300

    add metodo

--- DIO  Ruby Introdução a orientação a objetos/Conta_Bancaria/classes/conta_bancaria.rb
+++ DIO  Ruby Introdução a orientação a objetos/Conta_Bancaria/classes/conta_bancaria.rb
@@ -2,8 +2,12 @@
 #DIO - Ruby: Intodução a orientação a objetos - Exercidio prático: Conta Bancaria
 
 class ContaBancaria
-    def def initialize(proprietario, valor_inical)
-      @proprietario, @valor = proprietario, valor_inical
+    def def initialize(proprietario, valor_inicial)
+      @proprietario, @valor = proprietario, valor_inicial
+    end
+
+    def transferir(outra_conta, valor)
+    
     end
 end
 
