d822199ae9020016aafd2ee5564aa9464751b65b
Jean Porto <portojean@gmail.com>
2022-11-10 08:46:27 -0300

    feat: exercicio - Conta Bancária

---
+++ DIO  Ruby Introdução a orientação a objetos/Conta_Bancaria/principal.rb
@@ -0,0 +1,12 @@
+#https://web.dio.me/course/ruby-introducao-a-orientacao-a-objetos/learning/9ea794c1-eac3-4fdb-86d6-cc9acac3cae0
+#DIO - Ruby: Intodução a orientação a objetos - Exercidio prático: Conta Bancaria
+
+#principal
+
+conta_jean = ContaBancaria.new("Jean", 120)
+conta_porto = ContaBancaria.new("Porto", 200)
+
+conta_cadu.transferir(conta_porto, 50)
+
+p conta_jean.saldo   #50
+p conta_porto.saldo  #250
