f215cb5e4b45791f639954ea66ea6c26a4305bf7
Jean Porto <portojean@gmail.com>
2022-11-10 08:48:59 -0300

    alterando Conta Bancária

---
+++ DIO  Ruby Introdução a orientação a objetos/Conta_Bancaria/classes/conta_bancaria.rb
@@ -0,0 +1,9 @@
+#https://web.dio.me/course/ruby-introducao-a-orientacao-a-objetos/learning/9ea794c1-eac3-4fdb-86d6-cc9acac3cae0
+#DIO - Ruby: Intodução a orientação a objetos - Exercidio prático: Conta Bancaria
+
+class ContaBancaria
+    def def initialize(proprietario, valor_inical)
+      @proprietario, @valor = proprietario, valor_inical
+    end
+end
+
