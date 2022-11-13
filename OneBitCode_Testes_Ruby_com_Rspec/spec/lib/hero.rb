d0587310f4307301e524cfb6eedb6afc9d403a03
Jean Porto <portojean@gmail.com>
2022-11-13 19:21:10 -0300

    add cabeçalho

--- OneBitCode_Testes_Ruby_com_Rspec/spec/lib/hero.rb
+++ OneBitCode_Testes_Ruby_com_Rspec/spec/lib/hero.rb
@@ -1,3 +1,5 @@
+#https://www.youtube.com/watch?v=_h4jszFYkq4&list=PLdDT8if5attGc3fgFsFe5cvV9MwlVFNLW&index=4
+#Mini curso de teste Ruby com Rspec - Criando seu primeiro teste
 class Hero
     att_accessor :weapon
 
