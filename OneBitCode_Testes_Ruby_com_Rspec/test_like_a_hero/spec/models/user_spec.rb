3030b2889cf6cdeb8d5bf85c5068ab5d1f25b005
Jean Porto <portojean@gmail.com>
2022-11-14 10:40:56 -0300

    refactor: it level build da factory

--- OneBitCode_Testes_Ruby_com_Rspec/test_like_a_hero/spec/models/user_spec.rb
+++ OneBitCode_Testes_Ruby_com_Rspec/test_like_a_hero/spec/models/user_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is invalid if the level is not between 1 and 99" do
-    nikname = FFaker::Name.first_:name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(100..9999)
-    user = User.new(nikname: nikname, kind: kind, level: level)
-
+  it "is invalid if the level is not between 1 and 99" do 
+    user = build (:user, level: FFaker::Random.rand(100..9999))
     expect (user).to_not be_valid
   end
 
