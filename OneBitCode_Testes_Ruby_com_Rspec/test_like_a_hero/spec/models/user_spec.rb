e60bde4be688616161d93f00445c201b31685b58
Jean Porto <portojean@gmail.com>
2022-11-14 07:36:50 -0300

    add testes

--- OneBitCode_Testes_Ruby_com_Rspec/test_like_a_hero/spec/models/user_spec.rb
+++ OneBitCode_Testes_Ruby_com_Rspec/test_like_a_hero/spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is invalid if the level is not between 1 and 99"
-  it "returns the correct hero title"
+  it "is invalid if the level is not between 1 and 99" do
+    expect (User.create(nickname: 'Chronos', kind: :wizard, level: 90)).to_not be_valid
+  end
+  it "returns the correct hero title" do
+    user = User.create(nikname: 'Chronos', kind: :wizard, level: 1)
+    expect(user.title).to eq('wizard Chronos #1')
 end
