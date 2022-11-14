b2de49b157aed2978094f25fc7b584da4eccf827
Jean Porto <portojean@gmail.com>
2022-11-14 08:30:24 -0300

    refactor: it com FFaker

--- OneBitCode_Testes_Ruby_com_Rspec/test_like_a_hero/spec/models/user_spec.rb
+++ OneBitCode_Testes_Ruby_com_Rspec/test_like_a_hero/spec/models/user_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    expect (User.create(nickname: 'Chronos', kind: :wizard, level: 90)).to_not be_valid
+    nickname = FFaker::Name.first_:name
+    kind = %i[knight wizard].sample
+    level = FFaker::Random.rand(100..9999)
+    user = User.new(nickname: nickname, kind, level: level)
+
+    expect (user).to_not be_valid
   end
   it "returns the correct hero title" do
     user = User.create(nikname: 'Chronos', kind: :wizard, level: 1)
