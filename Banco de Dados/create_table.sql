57fe4106f9fe3015558d057c9db14ad37ca8c3b6
Jean Porto <portojean@gmail.com>
2022-11-22 19:09:01 -0300

    feat: create table filme

--- Banco de Dados/create_table.sql
+++ Banco de Dados/create_table.sql
@@ -9,4 +9,17 @@ CREATE TABLE tb_diretor(
 SELECT * FROM tb_diretor;
 
 
+--criando a tabela de filme
+CREATE TABLE tb_filme(
+    id_filme int NOT NULL,
+    nome_filme varchar(50),
+    id_diretor int NOT NULL,
+    id_produtora int NOT NULL,
+    genero varchar(25),
+    PRIMARY KEY (id_filme), 
+    FOREIGN KEY (id_diretor) REFERENCES tb_diretor(id_diretor)
+);
+
+--consultar a tabela de filme
+SELECT * FROM tb_filme;
 
