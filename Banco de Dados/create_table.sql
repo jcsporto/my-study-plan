16a15c92eb8c8c6357d2280bba564b95fd7c9892
Jean Porto <portojean@gmail.com>
2022-11-22 19:08:30 -0300

    feat: create table diretor

---
+++ Banco de Dados/create_table.sql
@@ -0,0 +1,12 @@
+--Criando a tabela de diretor
+CREATE TABLE tb_diretor(
+    id_diretor int NOT NULL,
+    nome_diretor varchar(50),
+    PRIMARY KEY (id_diretor)
+);
+
+--realizar consulta
+SELECT * FROM tb_diretor;
+
+
+
