2f8a62c0da902dcda1dd7e36c173584d194b6fb9
Jean Porto <portojean@gmail.com>
2022-11-24 06:33:56 -0300

    feat: incrementando def minimun_sum

--- Algoritmos Nivel 1/minimum_sum.rb
+++ Algoritmos Nivel 1/minimum_sum.rb
@@ -1,7 +1,24 @@
 #https://www.geeksforgeeks.org/minimum-sum-choosing-minimum-pairs-array/
 
 def minimum_sum(a)
+    #p "a = #{a}"
 
+    while a.length != 1
+        p "a tem um tamanho #{a.length}"
+        if a[0] > a[1]
+            a.delete_at(0)
+            b.append(a[1])
+            p "b é: #{b}"
+            p "b é: #{a}"
+        elsif a[0] < a[1]
+            a.delete_at(1)
+            b.append(a[0])
+            p "b é: #{b}"
+            p "b é: #{a}"
+        end
+    end
+    
+    p "Result is #{a[0]*b.length}"
 end
 
 a = [2, 4, 1, 3]
