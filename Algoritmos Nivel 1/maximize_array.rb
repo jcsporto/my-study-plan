0073ac694002f98d33d6b2d9ee351213937f3232
Jean Porto <portojean@gmail.com>
2022-12-02 20:24:05 -0300

    feat: add maximize_array

--- Algoritmos Nivel 1/maximize_array.rb
+++ Algoritmos Nivel 1/maximize_array.rb
@@ -1,8 +1,14 @@
 # https://www.geeksforgeeks.org/maximize-array-sun-after-k-negation-operations/
 
 def maximize_array(arr, k)
+
+    lem = arr.length
+    p lem
+    
  
 end
     
 arr = [-2, 0, 5, -1, 2]
 k = 4
+
+maximize_array(arr, k)
