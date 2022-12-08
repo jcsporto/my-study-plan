18a37b42dacb896e33ae5c51042a6104bcd7feed
Jean Porto <portojean@gmail.com>
2022-12-08 08:17:55 -0300

    feat: verifica par

--- Algoritmos Nivel 1/minimum_product.rb
+++ Algoritmos Nivel 1/minimum_product.rb
@@ -10,12 +10,23 @@ def minimum_product(a)
     count_z = 0
     (0..a.length-1).each do |i|
         if a[i]== 0
-            count_z = count_z + 1
+            count_z =  count_z + 1
         end       
     end
-    p count_z
-    
+   
+    # count number of even
+    count_e = 0
+    (0..a.length-1).each do |i|
+        if a[i]!= 0
+            if (a[i]).remainder(2) == 0
+                count_e =  count_e + 1
+            end
+        end       
+    end
+
+    p count_e
+
 end
 
-a = [1, 1, 19, 4, 3, 0]
+a = [1, 1, 19, 4, 6, 0]
 minimum_product(a)
