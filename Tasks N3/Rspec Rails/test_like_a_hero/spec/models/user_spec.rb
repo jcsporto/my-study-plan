6dc63266f478d01525a9aac8e6cb8469ae6a5ddc
Jean Porto <portojean@gmail.com>
2022-12-22 12:31:21 -0300

    feat: add it user spec

--- Tasks N3/Rspec Rails/test_like_a_hero/spec/models/user_spec.rb
+++ Tasks N3/Rspec Rails/test_like_a_hero/spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is invalid if the level is not between 1 and 99"
+  it "is invalid if the level is not between 1 and 99" do
+    expect(User.create(nickname: 'Chronos', kind: :wizard, level: 100)).to_not be_valid 
+  end  
+
   it "returns the correct hero title"
 end
