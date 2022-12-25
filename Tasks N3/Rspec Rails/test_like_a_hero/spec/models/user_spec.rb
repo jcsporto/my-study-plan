94a469ea5f32d71f67dea1119f16cb6e894e6a17
Jean Porto <portojean@gmail.com>
2022-12-25 11:01:07 -0300

    refactor: add ffacker in  it

--- Tasks N3/Rspec Rails/test_like_a_hero/spec/models/user_spec.rb
+++ Tasks N3/Rspec Rails/test_like_a_hero/spec/models/user_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    expect(User.create(nickname: 'Chronos', kind: :wizard, level: 100)).to_not be_valid 
+    nickname = FFaker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = FFaker::Random.rand(100..9999)
+    user = User.new( nickname: nickname, kind: kind, level: level)
+
+    expect(user).to_not be_valid 
   end  
 
   it "returns the correct hero title" do
