3ee19f93cd439212b71b9d797adf0e82319a2b31
Jean Porto <portojean@gmail.com>
2022-12-26 08:06:55 -0300

    refactor it - factory

--- Tasks N3/Rspec Rails/test_like_a_hero/spec/models/user_spec.rb
+++ Tasks N3/Rspec Rails/test_like_a_hero/spec/models/user_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    nickname = FFaker::Name.first_name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(100..9999)
-    user = User.new( nickname: nickname, kind: kind, level: level)
-
+    user = build( :user, level: FFaker::Random.rand(100..9999))
     expect(user).to_not be_valid 
   end  
 
