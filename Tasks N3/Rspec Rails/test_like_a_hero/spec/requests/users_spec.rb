bb65b47693ced08facca6359b5d0496c8cd9bfa5
Jean Porto <portojean@gmail.com>
2022-12-26 14:38:33 -0300

    feat: add exibição na view

--- Tasks N3/Rspec Rails/test_like_a_hero/spec/requests/users_spec.rb
+++ Tasks N3/Rspec Rails/test_like_a_hero/spec/requests/users_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
-      expect(response).to have_http_status(200)
+  describe "GET /index" do
+    it "returns http success" do
+      get "/users/index"
+      expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/users/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
