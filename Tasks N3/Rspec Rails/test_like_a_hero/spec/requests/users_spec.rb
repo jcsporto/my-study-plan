ff1580bd1a773f8bb5eeb94cfdcfc1bac4bcd3b5
Jean Porto <portojean@gmail.com>
2022-12-27 17:04:46 -0300

    feat: add test

--- Tasks N3/Rspec Rails/test_like_a_hero/spec/requests/users_spec.rb
+++ Tasks N3/Rspec Rails/test_like_a_hero/spec/requests/users_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "works! (now write some real specs)" do
+  describe "GET /users" do
+    it "returns success status" do
       get users_path
       expect(response).to have_http_status(200)
     end
+    it "the user's title is present"
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/users/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /users" do
+    context "when it has valid parameters" do
+      it "creates the user with correct attibutes"
+    end
+
+    context "when it has no valid parameters" do
+      it "does not create user"
     end
   end
-
 end
