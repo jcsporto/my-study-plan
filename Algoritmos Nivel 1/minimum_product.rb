6347d7d1305057e02a51eadcc61f854155c2286a
Jean Porto <portojean@gmail.com>
2022-12-31 12:08:07 -0300

    refactor: add class

--- Algoritmos Nivel 1/minimum_product.rb
+++ Algoritmos Nivel 1/minimum_product.rb
@@ -1,89 +1,42 @@
-# https://www.geeksforgeeks.org/minimum-product-subset-array/
+class Minimum_product
+    def run(a, n)
 
-def minimum_product(a)
+        max_neg = -4611686018427387903
+        min_pos = 4611686018427387903
 
-    # count number of zeros
-    count_z = 0
-    (0..a.length-1).each do |i|
-        if a[i]== 0
-            count_z =  count_z + 1
-        end       
-    end
-   
-    # count number of even
-    count_e = 0
-    (0..a.length-1).each do |i|
-        if a[i]!= 0
-            if (a[i]).remainder(2) == 0
-                count_e =  count_e + 1
+        conut_neg = 0
+        count_zero = 0
+
+        for i in 0..n - 1 do
+            if a[i].eq? 0
+                count_zero =  count_zero + 1
+                next
             end
-        end       
-    end
 
-    # count number of odd
-    count_o = a.length - count_e - count_z
-   
-
-    # count positives
-    count_p = 0
-    (0..a.length-1).each do |i|
-        if a[i] > 0
-            count_p = count_p + 1
-        end
-    end
-    
-    # count negatives
-    count_n =  0
-    (0..a.length-1).each do |i|
-        if a[i] < 0
-            count_n = count_n + 1
-            p count_n
-            p "We have #{count_n} negative numbers"
-        end
-    end
-
-    # Caso 3
-    if count_p == a.length
-        p "Sum is #{a.max}"
-        exit
-    end
-
-    # case 2
-    if (count_n).remainder(2) != 0
-        if count_z == 0
-            prod = 1
-            (0..a.length-1).each do |i|
-                prod =  prod*a[i]
+            if a[i] < 0
+                count_neg = count_neg + 1
+                max_neg = a.max
             end
+
+            if a[i] > 0
+                min_pos = a.min
+            end
+
+            prod = prod * a[i]
         end
-        print "The product is "
-        p prod
+
+        if count_zero == n || (count_neg == 0 && count_zero > 0)
+            return 0
+        end
+
+        if count_neg == 0
+            return min_pos
+        end
+
+        if !(count_neg & 1) && count_neg !=0
+            prod = prod /max_neg
     end
 
-    # case 1
-    if count_z == 0
-        if(count_n).remainder(2) == 0
-            (0..a.length-1).each do |i|
-                if a[i] == a.min
-                    a.delete_at(1)
-                end
-            end
-        end
-        prod = 1
-        (0..a.length-1).each do |i|
-            prod = prod*a[i]
-        end
-    end
-    
-    #case 4
-    if count_p > 1
-        if count_n == 0
-            if count_o != 0
-                p "The product is 0"
-            end
-        end
-    end
+    prod
+  end
 end
-
-a = [1, 1, -19, 4, 6, 0]
-minimum_product(a)
