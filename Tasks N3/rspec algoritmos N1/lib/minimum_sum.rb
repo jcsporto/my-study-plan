5bf0dc7317609a200d0bf78b3102cf3ae649360e
Jean Porto <portojean@gmail.com>
2022-12-31 17:44:20 -0300

    feat: add class minimum_sum

---
+++ Tasks N3/rspec algoritmos N1/lib/minimum_sum.rb
@@ -0,0 +1,29 @@
+
+class MinimumSum
+  def run(a, b, n, k)
+
+    diff = 0
+    res = 0
+
+    for i in 0..n - 1 do
+      pro = a[i] * b[i]
+      res = res + pro
+
+      if pro < 0 and b[i] < 0
+        temp = b[i] * (a[i] + 2 * k)
+      elsif pro < 0 and a[i] < 0
+        temp = b[i] * (a[i] - 2 * k)
+      elsif pro > 0 and a[i] < 0
+        temp = b[i] * (a[i] + 2 * k)
+      elsif pro > 0 and a[i] > 0
+        temp = b[i] * (a[i] - 2 * k)
+      end
+      d = (pro -temp).abs
+
+      if d > diff
+        diff = d
+      end
+    end
+    return res - diff
+  end
+end
