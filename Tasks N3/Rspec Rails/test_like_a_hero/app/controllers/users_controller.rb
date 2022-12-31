6e6f3bee013f61497439beff490d9cc9cc0e97b3
Jean Porto <portojean@gmail.com>
2022-12-31 11:29:53 -0300

    feat: add user_controller

--- Tasks N3/Rspec Rails/test_like_a_hero/app/controllers/users_controller.rb
+++ Tasks N3/Rspec Rails/test_like_a_hero/app/controllers/users_controller.rb
@@ -1,15 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
   end
 
   def create
-    @user = User.create(user_params)
-    redirect_to user_path
   end
-
-  private 
-    def user_params
-      params.require(:user).permit(:nickname, :kind, :level)
-    end
 end
