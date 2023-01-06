44b9862c88296c6150b013020448e7d82c33e69a
Jean Porto <portojean@gmail.com>
2023-01-06 07:50:53 -0300

    feat: add it in context whith jump

--- Tasks N3/rspec algoritmos N1/spec/jump_search_spec.rb
+++ Tasks N3/rspec algoritmos N1/spec/jump_search_spec.rb
@@ -19,4 +19,26 @@ RSpec.describe Jump do
       expect(result).to eq(-1)
     end
   end
+
+  context "whith jump" do
+    it "8" do
+      array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+      target = 55
+      jump = 4
+
+      result = Jump.new.run(array, target, jump)
+
+      expect(result).to eq(10)
+    end
+
+    it "-1" do
+      array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+      target = 1000
+      jump = 4
+
+      result = Jump.new.run(array, target, jump)
+
+      expect(result).to eq(-1)
+    end
+  end
 end
