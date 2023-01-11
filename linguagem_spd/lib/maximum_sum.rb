2979ad0510e4d640cf5978e2964b3844d7d18287
Jean Porto <portojean@gmail.com>
2023-01-11 08:15:58 -0300

    feat: add class maximum_sum

---
+++ linguagem_spd/lib/maximum_sum.rb
@@ -0,0 +1,14 @@
+class
+def run(array)
+  array.each_with_index do |item, index|
+    array[index] = item.abs if item.negative?
+  end
+
+  sum = 0
+  array.each do |item|
+    sum += item
+  end
+
+  sum
+end
+end
