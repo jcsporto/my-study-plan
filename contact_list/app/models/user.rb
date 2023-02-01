881f6cbef8d15db8a9405c319b112e9d6a7c1bce
Jean Porto <portojean@gmail.com>
2023-02-01 07:23:18 -0300

    fix: corrige a variável mocada

--- contact_list/app/models/user.rb
+++ contact_list/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+)*\.+[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: {maximum: 50}
   validates :password,  length: {maximum: 6}
