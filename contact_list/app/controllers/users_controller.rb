10286e1ccf0f84b86654d01d378db67d27eceb0e
Jean Porto <portojean@gmail.com>
2023-02-13 08:51:00 -0300

    refector: add def create in controller

--- contact_list/app/controllers/users_controller.rb
+++ contact_list/app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:sucess] = 'Usuário cadastrado com sucesso'
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
 end
