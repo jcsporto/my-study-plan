c59c5d8225e47e90b77f8834ac1aee17ea7dbe5b
Jean Porto <portojean@gmail.com>
2023-02-15 17:39:48 -0300

    feat: add

--- contact_list/app/controllers/sessions_controller.rb
+++ contact_list/app/controllers/sessions_controller.rb
@@ -3,15 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in(user)
-      redirect_to users_path(user)
+      redirect_to user_path(user)
     else
       flash.now[:danger] = "Email ou senha invalidos"
       render 'new'
     end
   end
 
-  def
+  def destroy
+    sing_out
+    flash[:success] = "Logout com sucesso"
+    redirect_to entrar_path
+  end
+
 end
