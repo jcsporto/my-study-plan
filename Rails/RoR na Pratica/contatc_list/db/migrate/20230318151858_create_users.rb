4058a3f992405b548dad75a8f2b75d7761a117b6
Jean Porto <portojean@gmail.com>
2023-03-18 15:36:58 -0300

    feat: controler user e  add def new e create

--- Rails/RoR na Pratica/contatc_list/db/migrate/20230318151858_create_users.rb
+++ Rails/RoR na Pratica/contatc_list/db/migrate/20230318151858_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :name
-      t.string :password_digest
+      t.string :email, null: false, default: ''
+      t.string :name, null: false, default: ''
+      t.string :password_digest, null: false, default: ''
 
       t.timestamps
     end
