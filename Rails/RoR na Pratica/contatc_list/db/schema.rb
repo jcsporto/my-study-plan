5b74991fbfdcebd6a3da7a32ca6f766b957d05b4
Jean Porto <portojean@gmail.com>
2023-03-19 09:54:13 -0300

    feat: add tabela user_to_contact

--- Rails/RoR na Pratica/contatc_list/db/schema.rb
+++ Rails/RoR na Pratica/contatc_list/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_18_151858) do
+ActiveRecord::Schema.define(version: 2023_03_19_125151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2023_03_18_151858) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2023_03_18_151858) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "contacts", "users"
 end
