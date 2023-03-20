aac9666823bbf9714359b0f7f4a391cbc25b67dd
Jean Porto <portojean@gmail.com>
2023-03-20 20:19:27 -0300

    feat: add pratica

---
+++ Rails/DIO/Coleções/each.rb
@@ -0,0 +1,15 @@
+# nomes = ['Maria', 'João', 'Marcelo']
+#
+# nome = 'Mariana'
+#
+# nomes.each do |nome|
+#   puts nome
+# end
+
+
+
+cursos = {'Curso 1' => 'Ruby', 'Curso 2' =>'GO', 'Curso 3' => 'Python',  'Curso 4' => 'Java'}
+
+cursos.each do |key, value|
+  puts "#{key} #{value}"
+end
