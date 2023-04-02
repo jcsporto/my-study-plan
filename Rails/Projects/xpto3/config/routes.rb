97349a6a2f6a6d0c2edc5ff13f6e5075f9ec1153
Jean Porto <portojean@gmail.com>
2023-04-02 11:24:55 -0300

    feat: add projeto xpto

--- Rails/Projects/xpto3/config/routes.rb
+++ Rails/Projects/xpto3/config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-
-  resources :ideas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
 
+  resources :ideas do
+    resources :comments
+  end
 
   root "pages#homepage"
   get 'pages/about'
