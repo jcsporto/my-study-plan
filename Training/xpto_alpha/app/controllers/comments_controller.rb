d179a5b1b8f7d793a5b0a2717295a6efd26faabb
Jean Porto <portojean@gmail.com>
2023-04-18 07:24:50 -0300

    fix: corrige message

--- Training/xpto_alpha/app/controllers/comments_controller.rb
+++ Training/xpto_alpha/app/controllers/comments_controller.rb
@@ -2,11 +2,12 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params) redirect_to article_path(@article)
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
   end
 
   private
-  def comments_params
+  def comment_params
     params.require(:comment).permit(:commenter, :body)
   end
 end
