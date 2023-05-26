f5665a328bf3f8c952b2c1a0cc8dfc9b69038159
Jean Porto <portojean@gmail.com>
2023-05-26 07:49:40 -0300

    refactor: add rota para comentários

--- Projects/xpta/config/routes.rb
+++ Projects/xpta/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # O metodo resources mapeia todas as rotas convencionais para uma coleção de recursos.
   # Por isso, subistituimos os gets por resources
-  resources :articles
-
+  resources :articles do
+    resources :comments
+    end
 end
