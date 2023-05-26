e8223b0fda87340cfb6987d8a75fe84c72a2a9e7
Jean Porto <portojean@gmail.com>
2023-05-26 07:20:27 -0300

    refactor: add destroy

--- Projects/xpta/app/controllers/articles_controller.rb
+++ Projects/xpta/app/controllers/articles_controller.rb
@@ -34,6 +34,14 @@ class ArticlesController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :body)
