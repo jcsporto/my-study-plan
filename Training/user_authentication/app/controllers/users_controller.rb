7ac3d25aeff8c0d968f8863fab75a5eeaf2ca5d1
Jean Porto <portojean@gmail.com>
2023-05-31 07:59:57 -0300

    refactor: add create e private

--- Training/user_authentication/app/controllers/users_controller.rb
+++ Training/user_authentication/app/controllers/users_controller.rb
@@ -3,5 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to @user, notice: "Usuário foi criado com sucesso!"
+      #tire o método de comentário quando criar o helper.
+      #Usuário depois de cadastrar-se acessa o sistema automaticamente
+      #sign_in(@user)
+    else
+      render action: new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
