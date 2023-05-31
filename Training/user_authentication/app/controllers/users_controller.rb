4f3044701abfdf2e40bfa2d37092721ee12161e6
Jean Porto <portojean@gmail.com>
2023-05-31 08:35:36 -0300

    refactor: add show

--- Training/user_authentication/app/controllers/users_controller.rb
+++ Training/user_authentication/app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
