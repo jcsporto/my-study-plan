f6925b41d64da464325f9033808050744de86842
Jean Porto <portojean@gmail.com>
2023-06-02 22:54:40 -0300

    refactor: reorganizando os diretórios

--- Tasks/Tasks N3/Rspec Rails/test_like_a_hero/app/javascript/packs/application.js
+++ Tasks/Tasks N3/Rspec Rails/test_like_a_hero/app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import Turbolinks from "../../.gem/ruby/2.7.0/gems/turbolinks-source-5.2.0/lib/assets/javascripts/turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
