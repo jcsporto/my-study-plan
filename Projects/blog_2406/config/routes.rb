472b13108cd12eeaf562faa53804e06984ba8572
Jean Porto <portojean@gmail.com>
2023-06-24 13:54:10 -0300

    refactor: add resources

--- Projects/blog_2406/config/routes.rb
+++ Projects/blog_2406/config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 
-
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
