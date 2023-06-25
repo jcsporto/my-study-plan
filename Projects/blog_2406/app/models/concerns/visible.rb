7321ce1394b27f646cacb2894ba9370a75c700db
Jean Porto <portojean@gmail.com>
2023-06-25 12:08:56 -0300

    refactor: add validates

--- Projects/blog_2406/app/models/concerns/visible.rb
+++ Projects/blog_2406/app/models/concerns/visible.rb
@@ -1,4 +1,12 @@
 module Visible
+  extend ActiveSupport::Concern
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  included do
+    validates :status, inclusion: { in: VALID_STATUSES }
+  end
+
   def archived?
     status == 'archived'
   end
