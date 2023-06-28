027ec655c792580a351d7c7d7c56fe2f541ad2e1
Jean Porto <portojean@gmail.com>
2023-06-27 21:30:00 -0300

    feat: add classe

---
+++ Ruby/Treino/Learn Ruby Programming/class.rb
@@ -0,0 +1,16 @@
+Class Vehicle{
+
+  Number no_of_wheels
+  Number horsepower
+  Characers type_of_tank
+  Number Capacity
+
+  Function speeding{
+  }
+
+  Function driving{
+  }
+
+  Function halting{
+  }
+}
