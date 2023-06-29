8e53fc535acebe029c28342b858308d3c6619df7
Jean Porto <portojean@gmail.com>
2023-06-28 21:51:35 -0300

    feat: add const

---
+++ Ruby/Treino/Learn Ruby Programming/const.rb
@@ -0,0 +1,8 @@
+MR_COUNT = 0         # constant defined on main Object class
+module Foo
+  MR_COUNT = 0
+  ::MR_COUNT = 1    # set global count to 1
+  MR_COUNT = 2      # set local count to 2
+end
+puts MR_COUNT        # this is the global constant
+puts Foo::MR_COUNT   # this is the local "Foo" constant
