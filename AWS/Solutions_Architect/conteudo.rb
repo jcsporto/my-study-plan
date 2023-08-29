35e8fcec211d96b2e34613429dab96acc37ee1a6
Jean Porto <portojean@gmail.com>
2023-08-29 20:15:18 -0300

    refactor: add até o ponto 2.2 de Escolha o desempenho de armazenamento e os bancos de dados

--- AWS/Solutions_Architect/conteudo.rb
+++ AWS/Solutions_Architect/conteudo.rb
@@ -35,7 +35,7 @@ https://aws.amazon.com/pt/developer/community/usergroups/?community-user-groups-
 
 BondeDaAWS 2.0
 
-Domínio 1:  Desenvolver a rquiteruas resilientes
+Domínio 1:  Desenvolver arquiteruas resilientes
 por Dan Rezende (AWS)
 
 
@@ -63,3 +63,111 @@ por Dan Rezende (AWS)
 1.4 Determine como desenvolver soluções de alta disponibilidade e/ou tolerantes a falhas
 
 
+
+
+Comentários feito por Henrique Ferreira  - Technical Trainer - AWS 
+  Instances Stores
+    - EBS vs efêmeros
+
+  
+
+
+
+    Entedemos que criou umas regras que estavam bloqueando o acesso ao backend, e que em seguida você as excluiu. Voce quer que criemos novas regras ou podemos finalizar o chamado? Quer falar em cal? Favor disponibilizar uma agenda. 
+
+
+
+
+    Prezado, 
+
+Em relação da regra customResponseCommon, podemos recriá-la com opção de count para que voce possa realizar as verificações necessárias? 
+
+Voce quer que criemos novamente as regras ou podemos finalizar o chamado? Quer falar em cal? Favor disponibilizar uma agenda. 
+
+Obs: 
+    
+
+  
+
+
+
+
+
+#######################################
+
+Domínio 2: Desenvolva arquiteturas de alto desempenho
+Vinicius 
+
+2.1 Escolha o desempenho de armazenamento e os bancos de dados
+
+      S3 
+      - Diferença das Classes
+          - Performace 
+          - Custos 
+          
+      Amazon S3 Transfer Acceleration 
+      Modelo de pagamento do S3 
+      Diferença entre armazenamento S3, Blocos e Arquivos
+
+      ####################################################
+      Banco de dados > RDS | DynamoDB | Redshift
+
+        Casos de uso do Amazon RDS
+          Usar o RDS:
+            - Transações ou consultas complexas 
+            - A consulta/gravação de média a alta taxa
+            - Não mais do que um único nó/shard de operador
+            - Alta Durabilidade
+
+          Não usar o RDS:
+            - Taxas de leitura/gravação massivas (por exemplo, 150 mil por gravação/segundo)
+            - Shading
+            - Solicitações e consultas GET/PUT simples 
+            - Personalização de RDBMS
+
+
+          Replicas de leitura do RDS  
+            - Desempenho e durabilidade aprimorados
+            - Aumentar a quantidade de formar elástica além da capacidade
+            - Tráfego de leitura de aplicação de alto Volumes
+            - Promover réplicas de leitura para idependente
+
+        DynamoDB ( Taxa de transfecência provisonada do DynamoDB)
+            - Alocação do recurso com base nos requisitos de capacidade de taxa de transferência de leitura/gravação
+            - Unidade de capacidade de leitura (para um item de até 4KB de tamanho)
+                - Uma leitura fortemente consistente por segundo
+                - Duas leituras eventualmente consistentes por segundo
+            - Unidade de capacidade de gravação (para um item de até 1KB de tamanho)
+                - Uma gravação por segundo    
+
+        
+
+2.2 Aplique o cache para melhorar o desempenho
+      
+    Apclicação do cache para melhorar o desempenho
+      parei em 5.16.03
+
+
+
+
+
+2.3 Desenolva soluções para obter elasticidade e escalabilidade
+
+2.4 Escolha recursos de design nas soluções que possibilitem a excelência operacional
+
+
+
+
+      
+        
+
+
+
+
+
+
+
+
+
+
+  
